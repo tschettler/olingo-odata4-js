4e4d5ee2d27cfd4fe6adc578778d95a0398bb334
Travis Schettler <travis_schettler@hotmail.com>
2019-10-26 01:34:31 -0500

    Added travis config

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,7 +121,8 @@ module.exports = function(grunt) {
   grunt.registerTask('clean', 'Clean the temporary build directories', ['priv-clean:build']);
 
   //    BUILD the odatajs library
-  grunt.registerTask('build', 'Build the odatajs library', ['clean:build','toBrowser:release', 'uglify:browser', 'copy:to-latest', 'nugetpack']);
+  grunt.registerTask('build', 'Build the odatajs library', ['clean:build','toBrowser:release', 'uglify:browser', 'copy:to-latest']);
+  grunt.registerTask('build:all', 'Build the odatajs library', ['clean:build','toBrowser:release', 'uglify:browser', 'copy:to-latest', 'nugetpack']);
 
   //    Create DOCumentation in /_build/doc
   grunt.registerTask('doc', 'Create documentation in folder ./doc',['clearEnv', 'jsdoc:src']);
--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 call npm install --g download-cli
 cd grunt-config\custom-tasks\rat
-call download "http://apache.mirrors.pair.com//creadur/apache-rat-0.12/apache-rat-0.12-bin.zip" --extract --out "_extern-tools"
+call download "https://archive.apache.org/dist/creadur/apache-rat-0.12/apache-rat-0.12-bin.zip" --extract --out "_extern-tools"
 call npm install
 ::call node "tasks\download.js"
 cd ..\..\..\
